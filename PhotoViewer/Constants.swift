86cf5cc8602b2a50b0d694e9a5515fe827ec8577
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2017-02-28 08:11:54 +0900

    Flickr fixed

--- PhotoViewer/Constants.swift
+++ PhotoViewer/Constants.swift
@@ -7,7 +7,7 @@
 //
 
 let kURLInstagram = "https://www.instagram.com/explore/tags"
-let kURLFlickr = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json"
+let kURLFlickr = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1"
 let kErrorDomain = "com.photoviewer.errordomain"
 let kSmallImageSize = "160x160"
 let kKeyFlickr = "beee19ea6c3aed0f0b44c169876233c5"
