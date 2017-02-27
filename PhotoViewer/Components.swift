0c23d9a161c7521b6b2f0310300cf96a8363892a
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2017-02-28 00:57:14 +0900

    adding method to donwload images on Components class

--- PhotoViewer/Components.swift
+++ PhotoViewer/Components.swift
@@ -7,7 +7,53 @@
 //
 
 import Foundation
+import UIKit
 
 typealias TMVoidCompletionHandler = () -> ()
 typealias TMSuceedCompletionHandler = (AnyObject?) -> ()
 typealias TMErrorCompletionHandler = (NSError) -> ()
+
+class Components
+{
+    class func downloadImageFrom(urlString: String, suceedHandler: TMSuceedCompletionHandler, failedHandler: TMErrorCompletionHandler)
+    {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
+            let url = NSURL(string: urlString)
+            if let url = url
+            {
+                let data = NSData(contentsOfURL: url)
+                if let data = data
+                {
+                    let image = UIImage(data: data)
+                    if let image = image
+                    {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            suceedHandler(image)
+                        })
+                    }
+                    else
+                    {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            let error = NSError(domain: kErrorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "Image could not be initialized from the specified data."])
+                            failedHandler(error)
+                        })
+                    }
+                }
+                else
+                {
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        let error = NSError(domain: kErrorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "No data returned from the server."])
+                        failedHandler(error)
+                    })
+                }
+            }
+            else
+            {
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    let error = NSError(domain: kErrorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "URL string malformed."])
+                    failedHandler(error)
+                })
+            }
+        }
+    }
+}
