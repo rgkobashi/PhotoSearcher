ee9c64a9cb46c303c4cace193a133e0a6d695029
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2017-02-27 23:10:18 +0900

    updating labels on search results

--- PhotoViewer/SearchResultsViewController.swift
+++ PhotoViewer/SearchResultsViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class SearchResultsViewController: UIViewController
 {
     @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var searchTermLabel: UILabel!
+    @IBOutlet weak var postsCountLabel: UILabel!
     
     var searchTerm: String!
     
@@ -24,8 +24,8 @@ class SearchResultsViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        titleLabel.text = ""
-        subtitleLabel.text = ""
+        searchTermLabel.text = ""
+        postsCountLabel.text = ""
         callInstagramTagService()
     }
     
@@ -41,13 +41,50 @@ class SearchResultsViewController: UIViewController
     {
         let service = InstagramSearchPostService(tag: searchTerm)
         Loader.show()
-        SessionManager.sharedInstance.start(service, suceedHandler: { (response) in
+        SessionManager.sharedInstance.start(service, suceedHandler: { [unowned self] (response) in
             Loader.dismiss()
-            print("response = \(response)")
+            if let response = response
+            {
+                let result = PostUtility.parseInstagramPostResponse(response)
+                self.searchTermLabel.text = "#\(result.name.lowercaseString)"
+                self.updatePostsCountLabel(result.count)
+                print("result.topPosts = \(result.topPosts)")
+                print("result.mostRecent = \(result.mostRecent)")
+            }
         }) { (error) in
             Loader.dismiss()
             print("error = \(error)")
             // TODO handler the errors
         }
+    }
+    
+    private func updatePostsCountLabel(count: Int)
+    {
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.groupingSeparator = ","
+        numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+
+        let boldAttributed: NSMutableAttributedString!
+        if let strCount = numberFormatter.stringFromNumber(count)
+        {
+            boldAttributed = NSMutableAttributedString(string: strCount, attributes: [NSFontAttributeName: UIFont.boldSystemFontOfSize(postsCountLabel.font.pointSize)])
+        }
+        else
+        {
+            boldAttributed = NSMutableAttributedString(string: "\(count)", attributes: [NSFontAttributeName: UIFont.boldSystemFontOfSize(postsCountLabel.font.pointSize)])
+        }
+        
+        let normalAttributed: NSAttributedString!
+        if count == 1
+        {
+            normalAttributed = NSAttributedString(string: " post")
+        }
+        else
+        {
+            normalAttributed = NSAttributedString(string: " posts")
+        }
+        
+        boldAttributed.appendAttributedString(normalAttributed)
+        postsCountLabel.attributedText = boldAttributed
     }
 }
