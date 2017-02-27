9c53bdd8146956ed226aff5d0868e25a06dc5b85
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2017-02-27 21:42:24 +0900

    TagUtility file added

---
+++ PhotoViewer/TagUtility.swift
@@ -0,0 +1,22 @@
+//
+//  TagUtility.swift
+//  PhotoViewer
+//
+//  Created by Rogelio Martinez Kobashi on 2/27/17.
+//  Copyright © 2017 Rogelio Martinez Kobashi. All rights reserved.
+//
+
+import Foundation
+
+class Tag
+{
+    
+}
+
+class TagUtility
+{
+    class func parseInstagramTagResponse(response: AnyObject) -> [Tag]
+    {
+        
+    }
+}
