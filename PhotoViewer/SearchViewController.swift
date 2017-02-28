5a69a67f6a56ea1a3bb4cdfb7729a10c30a26cbf
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2017-02-28 23:58:06 +0900

    adding topView to searchViewController

--- PhotoViewer/SearchViewController.swift
+++ PhotoViewer/SearchViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SearchViewController: UIViewController
 {
+    @IBOutlet weak var topView: UIView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var tableViewBottomConstraint: NSLayoutConstraint!
