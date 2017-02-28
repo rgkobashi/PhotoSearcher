7952874bf1215aa07ea06ff059a10205563423fd
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2017-03-01 06:21:10 +0900

    photo class converted to protocol

--- PhotoViewer/Photo.swift
+++ PhotoViewer/Photo.swift
@@ -9,31 +9,9 @@
 import Foundation
 import UIKit
 
-class Photo
+protocol Photo
 {
-    var text = ""
-    var thumbnailUrl = ""
-    var originalUrl = ""
-    var thumbnailImage: UIImage?
-    var originalImage: UIImage?
-    
-    func downloadThumbnailImage(_ suceedHandler: @escaping VoidCompletionHandler, failedHandler: @escaping ErrorCompletionHandler)
-    {
-        Utilities.downloadImageFrom(thumbnailUrl, suceedHandler: { [weak self] (result) in
-            self?.thumbnailImage = result as? UIImage
-            suceedHandler()
-        }, failedHandler: { (error) in
-            failedHandler(error)
-        })
-    }
-    
-    func downloadOriginalImage(_ suceedHandler: @escaping VoidCompletionHandler, failedHandler: @escaping ErrorCompletionHandler)
-    {
-        Utilities.downloadImageFrom(originalUrl, suceedHandler: { [weak self] (result) in
-            self?.originalImage = result as? UIImage
-            suceedHandler()
-        }, failedHandler: { (error) in
-            failedHandler(error)
-        })
-    }
+    var text: String  { get set }
+    var thumbnailUrl: String  { get set }
+    var originalUrl: String  { get set }
 }
