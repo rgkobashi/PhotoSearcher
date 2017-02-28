fc971b8b09e6f8a77994a934a6e66587604f1d06
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2017-03-01 01:06:08 +0900

    adding assets fro about screen

---
+++ PhotoViewer/AboutViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.swift
+//  PhotoViewer
+//
+//  Created by Rogelio Martinez Kobashi on 3/1/17.
+//  Copyright © 2017 Rogelio Martinez Kobashi. All rights reserved.
+//
+
+import Cocoa
+
+class AboutViewController: UIViewController {
+
+}
